74bcad2030b3c641a69c1275bd608fe5ee74ec09
spaceadh <spacesealadhesives13@gmail.com>
2023-12-15 20:09:12 -0800

    FinallyCrackedtheSQLPart.Hurray

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,8 @@
--- Prepares the MySQL server for this project
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
