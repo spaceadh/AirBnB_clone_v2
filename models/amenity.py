38b3181db363c50f259c6e22d73c0b9358271e6a
spaceadh <spacesealadhesives13@gmail.com>
2023-12-21 19:28:51 -0800

    changesonAmenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,15 @@
+#!/usr/bin/python3
+""" Amenity Module for HBNB project """
+import os
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
 from models.base_model import BaseModel, Base
 
+
 class Amenity(BaseModel, Base):
-    """The Amenity class, contains the amenity name"""
-
+    """Represents an amenity data set."""
     __tablename__ = 'amenities'
-
-    name = Column(String(128), nullable=False)
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
